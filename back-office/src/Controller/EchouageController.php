60b59a755fdccb8dfe6f6c20ef2d22d905e537d8
Morgan Van Amerongen <vanamerongen.morgan@gmail.com>
2022-01-11 12:10:27 +0100

    css for back-office

--- back-office/src/Controller/EchouageController.php
+++ back-office/src/Controller/EchouageController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/echouage")
+ * @Route("/back-office/echouage")
  */
 class EchouageController extends AbstractController {
     /**
